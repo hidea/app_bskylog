46f9a2e15a701251c6697a27506dac3085f995f5
hidea <jca03271@nifty.com>
2025-02-20 08:01:30 +0900

    database schema v3 and puv upgrade

--- lib/database.steps.dart
+++ lib/database.steps.dart
@@ -121,8 +121,83 @@ i1.GeneratedColumn<bool> _column_11(String aliasedName) =>
 i1.GeneratedColumn<String> _column_12(String aliasedName) =>
     i1.GeneratedColumn<String>('post', aliasedName, false,
         type: i1.DriftSqlType.string);
+
+final class Schema3 extends i0.VersionedSchema {
+  Schema3({required super.database}) : super(version: 3);
+  @override
+  late final List<i1.DatabaseSchemaEntity> entities = [
+    posts,
+  ];
+  late final Shape1 posts = Shape1(
+      source: i0.VersionedTable(
+        entityName: 'posts',
+        withoutRowId: false,
+        isStrict: false,
+        tableConstraints: [],
+        columns: [
+          _column_0,
+          _column_13,
+          _column_2,
+          _column_3,
+          _column_4,
+          _column_5,
+          _column_6,
+          _column_7,
+          _column_8,
+          _column_9,
+          _column_10,
+          _column_11,
+          _column_12,
+          _column_14,
+        ],
+        attachedDatabase: database,
+      ),
+      alias: null);
+}
+
+class Shape1 extends i0.VersionedTable {
+  Shape1({required super.source, required super.alias}) : super.aliased();
+  i1.GeneratedColumn<int> get id =>
+      columnsByName['id']! as i1.GeneratedColumn<int>;
+  i1.GeneratedColumn<String> get feedAuthorDid =>
+      columnsByName['feed_author_did']! as i1.GeneratedColumn<String>;
+  i1.GeneratedColumn<String> get uri =>
+      columnsByName['uri']! as i1.GeneratedColumn<String>;
+  i1.GeneratedColumn<String> get authorDid =>
+      columnsByName['author_did']! as i1.GeneratedColumn<String>;
+  i1.GeneratedColumn<DateTime> get indexed =>
+      columnsByName['indexed']! as i1.GeneratedColumn<DateTime>;
+  i1.GeneratedColumn<String> get replyDid =>
+      columnsByName['reply_did']! as i1.GeneratedColumn<String>;
+  i1.GeneratedColumn<bool> get havEmbedImages =>
+      columnsByName['hav_embed_images']! as i1.GeneratedColumn<bool>;
+  i1.GeneratedColumn<bool> get havEmbedExternal =>
+      columnsByName['hav_embed_external']! as i1.GeneratedColumn<bool>;
+  i1.GeneratedColumn<bool> get havEmbedRecord =>
+      columnsByName['hav_embed_record']! as i1.GeneratedColumn<bool>;
+  i1.GeneratedColumn<bool> get havEmbedRecordWithMedia =>
+      columnsByName['hav_embed_record_with_media']! as i1.GeneratedColumn<bool>;
+  i1.GeneratedColumn<bool> get havEmbedVideo =>
+      columnsByName['hav_embed_video']! as i1.GeneratedColumn<bool>;
+  i1.GeneratedColumn<bool> get reasonRepost =>
+      columnsByName['reason_repost']! as i1.GeneratedColumn<bool>;
+  i1.GeneratedColumn<String> get post =>
+      columnsByName['post']! as i1.GeneratedColumn<String>;
+  i1.GeneratedColumn<DateTime> get retrieved =>
+      columnsByName['retrieved']! as i1.GeneratedColumn<DateTime>;
+}
+
+i1.GeneratedColumn<String> _column_13(String aliasedName) =>
+    i1.GeneratedColumn<String>('feed_author_did', aliasedName, false,
+        type: i1.DriftSqlType.string,
+        defaultValue: const CustomExpression('\'\''));
+i1.GeneratedColumn<DateTime> _column_14(String aliasedName) =>
+    i1.GeneratedColumn<DateTime>('retrieved', aliasedName, false,
+        type: i1.DriftSqlType.dateTime,
+        defaultValue: const CustomExpression('1735657200'));
 i0.MigrationStepWithVersion migrationSteps({
   required Future<void> Function(i1.Migrator m, Schema2 schema) from1To2,
+  required Future<void> Function(i1.Migrator m, Schema3 schema) from2To3,
 }) {
   return (currentVersion, database) async {
     switch (currentVersion) {
@@ -131,6 +206,11 @@ i0.MigrationStepWithVersion migrationSteps({
         final migrator = i1.Migrator(database, schema);
         await from1To2(migrator, schema);
         return 2;
+      case 2:
+        final schema = Schema3(database: database);
+        final migrator = i1.Migrator(database, schema);
+        await from2To3(migrator, schema);
+        return 3;
       default:
         throw ArgumentError.value('Unknown migration from $currentVersion');
     }
@@ -139,8 +219,10 @@ i0.MigrationStepWithVersion migrationSteps({
 
 i1.OnUpgrade stepByStep({
   required Future<void> Function(i1.Migrator m, Schema2 schema) from1To2,
+  required Future<void> Function(i1.Migrator m, Schema3 schema) from2To3,
 }) =>
     i0.VersionedSchema.stepByStepHelper(
         step: migrationSteps(
       from1To2: from1To2,
+      from2To3: from2To3,
     ));
